e93128c5aef794fb78bcae3893a7ba6e3467d97d
Peter <petersolomon704@gmail.com>
2025-01-07 23:09:58 +0100

    Modal
    Added a modal component

---
+++ src/Components/modal/Modal.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface ModalProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  children: React.ReactNode;
+}
+
+export const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  closeModal,
+  children,
+}) => {
+  if (!isOpen) return null;
+
+  return (
+    <ModalWrapper>
+      <ModalContent>
+        <CloseButton onClick={closeModal}>X</CloseButton>
+        {children}
+      </ModalContent>
+      <Overlay onClick={closeModal} />
+    </ModalWrapper>
+  );
+};
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.5);
+`;
+
+const ModalContent = styled.div`
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  position: relative;
+  max-width: 500px;
+  width: 100%;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: none;
+  border: none;
+  font-size: 20px;
+  cursor: pointer;
+`;
